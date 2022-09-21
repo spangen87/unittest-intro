8da02f2cbd39c4aaabdae4a29868bc1cf230cec2
spangen87 <rikard.spangmyr@bestlas.se>
2022-09-21 14:05:00 +0000

    Add first tests

--- evens.py
+++ evens.py
@@ -1,6 +1,24 @@
 def even_number_of_evens(numbers):
-    return None
     
+    if isinstance(numbers, list):
+        if numbers == []:
+            return False
+        else:
+            evens = 0
+        for n in numbers:
+            if n % 2 == 0:
+                evens += 1   
+        
+        if evens:
+            return evens % 2 == 0
+        else:
+            return False                 
+    else:
+        raise TypeError("A list was not passed into the function")    
+
+
+
+
 
 if __name__ == '__main__':
     print(even_number_of_evens(5))    
