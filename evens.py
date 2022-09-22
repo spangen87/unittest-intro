3e45a882b0bf63fb86f3bf475fbeab24c2cd531d
spangen87 <rikard.spangmyr@bestlas.se>
2022-09-22 13:44:02 +0000

    Add students and test_students

--- evens.py
+++ evens.py
@@ -1,20 +1,10 @@
 def even_number_of_evens(numbers):
     
     if isinstance(numbers, list):
-        if numbers == []:
-            return False
-        else:
-            evens = 0
-        for n in numbers:
-            if n % 2 == 0:
-                evens += 1   
-        
-        if evens:
-            return evens % 2 == 0
-        else:
-            return False                 
+        evens = sum([1 for n in numbers if n % 2 == 0])      
+        return True if evens and evens % 2 == 0 else False              
     else:
-        raise TypeError("A list was not passed into the function")    
+        raise TypeError("A list was not passed into the function")
 
 
 
